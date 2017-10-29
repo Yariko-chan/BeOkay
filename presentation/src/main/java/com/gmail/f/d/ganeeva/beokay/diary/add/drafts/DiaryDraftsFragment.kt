31f9920de86f5e6cf21381ac05699b35fa60c9fc
D.Ganeeva <lililala1991@gmail.com>
2017-10-29 17:59:05 +0300

    diary drafts dialog interface and appearance

--- presentation/src/main/java/com/gmail/f/d/ganeeva/beokay/diary/add/drafts/DiaryDraftsFragment.kt
+++ presentation/src/main/java/com/gmail/f/d/ganeeva/beokay/diary/add/drafts/DiaryDraftsFragment.kt
@@ -1,5 +1,6 @@
 package com.gmail.f.d.ganeeva.beokay.diary.add.drafts
 
+import android.app.ActionBar
 import android.app.Dialog
 import android.databinding.DataBindingUtil
 import android.os.Bundle
@@ -12,6 +13,9 @@ import android.view.ViewGroup
 import com.gmail.f.d.ganeeva.beokay.R
 import com.gmail.f.d.ganeeva.beokay.base.BaseDialogFragment
 import com.gmail.f.d.ganeeva.beokay.databinding.FragmentDiaryDraftsBinding
+import android.view.WindowManager
+
+
 
 /**
  * Created by Diana on 18.09.2017 at 8:11.
@@ -38,12 +42,16 @@ class DiaryDraftsFragment : BaseDialogFragment() {
     }
 
     override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
-        return super.onCreateDialog(savedInstanceState)
-
         val builder = Dialog(activity)
-        val width = resources.getDimensionPixelSize(R.dimen.popup_width)
-        val height = resources.getDimensionPixelSize(R.dimen.popup_height)
-        builder.window.setLayout(width, height)
         builder.window.attributes.windowAnimations = R.style.DiaryDraftsDialogAnimation
+        return builder
+    }
+
+    override fun onStart() {
+        super.onStart()
+        getDialog().getWindow().setLayout(
+                getResources().getDisplayMetrics().widthPixels*5/6,
+                getResources().getDisplayMetrics().heightPixels*3/4
+        );
     }
 }
